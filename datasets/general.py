efde027e9cb2aa3a4c1cf492b2721d2219c2bf8f
sallamander <ssall@alumni.nd.edu>
2016-07-27 16:45:51 -0500

    Add function to generate multivariate linear data

--- datasets/general.py
+++ datasets/general.py
@@ -7,7 +7,7 @@ processing or natural language processing.
 import numpy as np
 
 def gen_simple_linear(beta_0, beta_1, n_obs): 
-    """Generate data that follows a y = beta_0 + beta_1 * x_1 relationship.
+    """Generate data that follows a univariate linear relationship.
 
     Args:
     ----
@@ -23,5 +23,31 @@ def gen_simple_linear(beta_0, beta_1, n_obs):
     
     xs = np.random.random(size=(n_obs, 1))
     ys = beta_0 + beta_1 * xs
+
+    return xs, ys
+
+def gen_multiple_linear(betas, n_obs): 
+    """Generate data that follows a multivariate linear relationship. 
+    
+    This function assumes that the first `beta` in the `betas` corresponds
+    to `beta_0`, and will associate a vector of 1's with this `beta`. 
+
+    Args:
+    ----
+        betas: 1d np.ndarray of ints/floats
+        n_obs: int
+
+    Return:
+    ------
+        ys: 2d np.ndarray
+        xs: 1d np.ndarray
+    """
+    
+    n_vars = len(betas) - 1
+    xs = np.random.random(size=(n_obs, n_vars))
+    ones = np.ones(shape=(n_obs, 1))
+    xs = np.concatenate([ones, xs], axis=1)
+
+    ys = np.dot(xs, betas)
 
     return xs, ys
